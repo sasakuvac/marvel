f31e314888532c4c2229989073ce4ffcf93fb178
petrovichh <petrovichjelena99@gmail.com>
2021-03-11 22:27:16 +0100

    fixed results section

--- index.js
+++ index.js
@@ -6,9 +6,8 @@ http://gateway.marvel.com/v1/public/characters?ts=1&apikey=2bdacb7a1f4583f33e420
 
 
 let input = document.querySelector('input');
-let resultsBox = document.querySelector('.results');
+let resultsBox = document.querySelector('.results-section');
 let button = document.querySelector('button');
-let characterBox = document.querySelector('article');
 
 
    // function fetchCharacters() {
@@ -27,89 +26,41 @@ let characterBox = document.querySelector('article');
         function test() {
         for (let i = 0; i < charactersList.length; i++) {
 
-
+            // selecting character images & adding class name
             let marvelImg = document.createElement('img'); 
             marvelImg.className += 'char-images';
             marvelImg.src = charactersList[i].thumbnail.path + '.' + charactersList[i].thumbnail.extension;
             
-
+            // selecting character names & adding class name
             let marvelHeading = document.createElement('h3');
+            marvelHeading.className += 'marvel-heading'; 
             marvelHeading.innerHTML = charactersList[i].name;
 
-            
-            characterBox.className = 'character-box';
+            // creating marvelHeadingBox
+            let marvelHeadingBox = document.createElement('div');
+            marvelHeadingBox.className += 'marvel-heading-box';
+
+            // creating article element
+            let characterBox = document.createElement('article');
+            characterBox.className += 'character-box';
+
+            // adding elements
+            resultsBox.appendChild(characterBox);
             characterBox.appendChild(marvelImg);
-            characterBox.appendChild(marvelHeading);
+            characterBox.appendChild(marvelHeadingBox);
+            marvelHeadingBox.appendChild(marvelHeading);
 
-
+            // temporary code
             if (charactersList[i] === charactersList[9]) {
                 break;
-            }
-
-            /*if (charactersList[i] === charactersList[9]) {
-                break;
-            }*/
-            
-
-            
+            }    
         }
 
     }
     })
-//}
- 
-/*
-
- <article class="marvel-character-box">
-                <img src="${marvelImg}" class="marvel-img">
-                <h3 class="marvel-heading"> ${marvelCharacters[i].title} </h3>
-                <span class="marvel-id">${marvelCharacters[i].id}</span>
-                <span class="marvel-bookmark">&hearts;</span>
-                <span class="bookmark--hidden">X</span>
-                </article>
-
-
-
-marvelImg = marvelCharacters[i].thumbnail.path + '.' + marvelCharacters[i].thumbnail.extension;
-
-
-document.addEventListener('DOMContentLoaded', loadContent);
-
-*/
-    
-
-    
 
 
 
 
 
-/*
-let characters = data['data']['results'];
-      
-        console.log(characters)
 
-         console.log(data['data'])
-*/
-
-
-
-
-/*
-
- <article class="marvel-character-box">
-                <img src="${marvelImg}" class="marvel-img">
-                <h3 class="marvel-heading"> ${marvelCharacters[i].title} </h3>
-                <span class="marvel-id">${marvelCharacters[i].id}</span>
-                <span class="marvel-bookmark">&hearts;</span>
-                <span class="bookmark--hidden">X</span>
-                </article>
-
-
-
-marvelImg = marvelCharacters[i].thumbnail.path + '.' + marvelCharacters[i].thumbnail.extension;
-
-
-document.addEventListener('DOMContentLoaded', loadContent);
-
-*/
